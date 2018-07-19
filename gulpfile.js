55fe0ea1438d85cf78154a3e23ec121cb34f588d
moonbyt3 <miloradjekic@outlook.com>
2018-07-19 02:47:46 +0200

    added gulp-iconfont updated folder architecture minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	iconfont         = require('gulp-iconfont'),
+	iconfontCss      = require('gulp-iconfont-css'),
+	sass 						 = require('gulp-sass');
 
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+var runTimestamp = Math.round(Date.now()/1000);
 
 gulp.task('sass', function(){
   return gulp.src('app/assets/sass/*.scss')
@@ -10,15 +13,34 @@ gulp.task('sass', function(){
     .pipe(gulp.dest('dist/stylesheets/css'))
 });
 
+//icon fonts
+gulp.task('iconfont', function(){
+  gulp.src(['app/assets/svg/*.svg'])
+    .pipe(iconfontCss({
+      fontName: 'font-icons',
+      path: 'app/assets/sass/templates/_icon-font.scss', // where is template for updateing sass file
+      targetPath: '../../assets/sass/base/_icon-font.scss', //where to update sass file
+			fontPath: '../../assets/fonts/' // where are fonts exported
+    }))
+    .pipe(iconfont({
+			fontName: 'font-icons',
+      formats: ['ttf', 'eot', 'woff'], // The font file formats that will be created
+      normalize: true,
+			timestamp: runTimestamp
+     }))
+    .pipe(gulp.dest('app/assets/fonts/'));
+});
+
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
       baseDir: 'app'
     },
   })
-})
+});
 
-gulp.task('watch', ['sass', 'browserSync'], function (){
+
+gulp.task('watch', [ 'sass', 'browserSync'], function (){
   gulp.watch('app/assets/sass/*.scss', ['sass']); 
   // Reloads the browser whenever HTML, JS or SCSS files change
   gulp.watch('app/*.html', browserSync.reload); 
